1d245b1b030f9e339b3038600fbf4ffd1fd06e73
Travis <tsahadeo@mac.com>
2022-04-04 17:25:44 -0400

    commit tree fe16e1a291a6b69ce180d7076321cdbe27f23bce

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const backWord = word.toLowerCase().split("").reverse().join("") 
+  if (backWord === word) {
+    return true
+  } else {
+    return false
+  }
 }
-
 /* 
   Add your pseudocode here
-*/
+take in a word (argument),
+reverse the letters,
+then compare that reversed word to the original argument.
+  */
 
 /*
   Add written explanation of your solution here
+  the function takes a word as an argument, 
+  then converts the letters to lowercase, 
+  reverses the order of the letters,
+  then compares it to the original argument.
+  if the values are the same the function returns true;
+  if the values are not the same the function return false.
 */
 
 // You can run `node index.js` to view these console logs
